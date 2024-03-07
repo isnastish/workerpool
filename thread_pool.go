c12cd56c8adcb012a378dd892ca8cf18647f2866
ayevtushenko <alexey.yevtushenko@ubisoft.com>
2024-03-07 15:07:54 +0100

    Introduced metrics for worker pool.

--- thread_pool.go
+++ thread_pool.go
@@ -5,26 +5,35 @@ import (
 	"runtime"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
-type ThreadPool struct {
-	waitingQueue      *ThreadSafeQueue[func()]
-	tasksQueue        *ThreadSafeQueue[func()]
-	threadCount       atomic.Int32 // atomic since will be accessed by multiple threads (workers).
-	maxThreads        int32
-	running           bool
-	tasksSubmitted    int32 // metrics
-	waitingTasksCount int32 // metrics
-
-	wg sync.WaitGroup
+type Metrics struct {
+	tasksSubmitted  uint32
+	tasksDone       uint32
+	tasksQueued     uint32
+	threadsSpawned  uint32
+	threadsFinished uint32
 }
 
-func NewPool(numThreads ...int32) *ThreadPool {
+type ThreadPool struct {
+	maxThreads     uint32
+	waitingQueue   *ThreadSafeQueue[func()]
+	tasksQueue     *ThreadSafeQueue[func()]
+	metrics        Metrics
+	displayMetrics bool
+	wg             sync.WaitGroup
+	threadCount    uint32
+
+	running bool
+}
+
+func NewPool(displayMetrics bool, numThreads ...uint32) *ThreadPool {
 	// Get a number of cores usable by the current process.
 	// This is equivalent to maximum amount of goroutines (workers) created.
-	hardwareCPU := int32(runtime.NumCPU())
+	hardwareCPU := uint32(runtime.NumCPU())
 
-	var maxThreads int32
+	var maxThreads uint32
 	if len(numThreads) > 0 {
 		if numThreads[0] < 1 || numThreads[0] > hardwareCPU {
 			maxThreads = hardwareCPU
@@ -35,11 +44,13 @@ func NewPool(numThreads ...int32) *ThreadPool {
 		maxThreads = hardwareCPU
 	}
 
+	const threadSafe = true
 	p := &ThreadPool{
-		waitingQueue: NewQueue[func()](),
-		tasksQueue:   NewQueue[func()](),
-		maxThreads:   maxThreads,
-		wg:           sync.WaitGroup{},
+		waitingQueue:   NewQueue[func()](threadSafe),
+		tasksQueue:     NewQueue[func()](threadSafe),
+		maxThreads:     maxThreads,
+		displayMetrics: displayMetrics,
+		wg:             sync.WaitGroup{},
 	}
 
 	return p
@@ -47,17 +58,22 @@ func NewPool(numThreads ...int32) *ThreadPool {
 
 func (p *ThreadPool) SubmitTask(task func()) {
 	if nil == task {
-		fmt.Println("WARNING: nil task was submitted.")
+		fmt.Println("WARNING: nil task was rejected.")
 		return
 	}
 
 	// Submit tasks into a tasksQueue for later execution by workers.
 	p.tasksQueue.Push(task)
-	p.tasksSubmitted++
+	p.metrics.tasksSubmitted++
 }
 
 func (p *ThreadPool) ProcessSubmittedTasks() {
 	p.running = true
+
+	if p.tasksQueue.Empty() {
+		fmt.Println("INFO: No tasks submitted.")
+		return
+	}
 
 	for p.running {
 		// Pop tasks from a tasksQueue, if the amount of spawned goroutines less than
@@ -71,24 +87,26 @@ func (p *ThreadPool) ProcessSubmittedTasks() {
 		if p.tasksQueue.TryPop(&task) {
 			// If we exceeded the amount of all available workers,
 			// put task into a waiting queue for further processing.
-			if p.threadCount.Load() < p.maxThreads {
-				p.wg.Add(1)
-				go p.worker(task)
-				p.threadCount.Add(1)
+			if atomic.LoadUint32(&p.threadCount) < p.maxThreads {
+				p.spawnWorker(task)
 			} else {
 				p.waitingQueue.Push(task)
-				p.waitingTasksCount++
+				// This is only indicative since the same element will be pushed over and over again
+				// if all the workers are busy.
+				// p.metrics.tasksQueued++
 			}
 		} else {
 			if p.waitingQueue.TryPop(&task) {
 				// Check if there are workers available,
 				// if not, put the task back into a waiting queue.
-				if p.threadCount.Load() < p.maxThreads {
-					p.wg.Add(1)
-					go p.worker(task)
-					p.threadCount.Add(1)
+				if atomic.LoadUint32(&p.threadCount) < p.maxThreads {
+					p.spawnWorker(task)
 				} else {
+					// Sleep for half a second if all the workers are busy,
+					// before pushing a task back to the waiting queue.
+					time.Sleep(500 * time.Millisecond)
 					p.waitingQueue.Push(task)
+					p.metrics.tasksQueued++
 				}
 			} else {
 				// Both tasks and waiting queues are empty
@@ -100,17 +118,47 @@ func (p *ThreadPool) ProcessSubmittedTasks() {
 
 	// Wait for all spawned workers to finish their work.
 	p.wg.Wait()
+
+	// Display accumulated metrics.
+	if p.displayMetrics {
+		info := fmt.Sprintf(
+			"Metrics:\n"+
+				"\tTasks submitted:  %d\n"+
+				"\tTasks done:       %d\n"+
+				"\tTasks queued:     %d\n"+
+				"\tThreads spawned:  %d\n"+
+				"\tThreads finished: %d\n",
+			p.metrics.tasksSubmitted,
+			p.metrics.tasksDone,
+			p.metrics.tasksQueued,
+			p.metrics.threadsSpawned,
+			p.metrics.threadsFinished,
+		)
+		fmt.Println(info)
+	}
+}
+
+func (p *ThreadPool) spawnWorker(task func()) {
+	// Create a worker and assign a task for it to execute.
+	p.wg.Add(1)
+	go p.worker(task)
+	atomic.AddUint32(&p.threadCount, 1)
+	p.metrics.threadsSpawned++
 }
 
 func (p *ThreadPool) worker(task func()) {
 	for task != nil {
 		task()
+		atomic.AddUint32(&p.metrics.tasksDone, 1)
 		if !p.tasksQueue.TryPop(&task) {
 			task = nil
 		}
 	}
+
 	// Decrement threads count so other workers can be spawned,
 	// in case the waiting queue is not empty and waiting for at least one worker to complete.
-	p.threadCount.Add(-1)
+	atomic.AddUint32(&p.threadCount, ^uint32(0))
+	atomic.AddUint32(&p.metrics.threadsFinished, 1)
+
 	p.wg.Done()
 }
--- thread_pool_test.go
+++ thread_pool_test.go
@@ -9,34 +9,37 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestThreadPool_TooHighWorkerCount(t *testing.T) {
-	const THREADS_COUNT = 256
-	var expectedThreadCount = int32(runtime.NumCPU())
+const displayMetrics = false
 
-	p := NewPool(THREADS_COUNT)
+func TestThreadPool_TooHighWorkerCount(t *testing.T) {
+	const threadCount = 256
+	var expectedThreadCount = uint32(runtime.NumCPU())
+
+	p := NewPool(displayMetrics)
 	assert.Equal(t, p.maxThreads, expectedThreadCount)
 }
 
 func TestThreadPool_NegativeWorkerCount(t *testing.T) {
-	var expectedThreadCount = int32(runtime.NumCPU())
-	p := NewPool(-256)
+	const threadCount = 256
+	var expectedThreadCount = uint32(runtime.NumCPU())
+	p := NewPool(displayMetrics, threadCount)
 	assert.Equal(t, p.maxThreads, expectedThreadCount)
 }
 
 func TestThreadPool_CorrectWorkerCount(t *testing.T) {
-	const THREADS_COUNT = 16
-	p := NewPool(THREADS_COUNT)
-	assert.EqualValues(t, p.maxThreads, THREADS_COUNT)
+	const threadCount = 16
+	p := NewPool(displayMetrics, threadCount)
+	assert.EqualValues(t, p.maxThreads, threadCount)
 }
 
 func TestThreadPoo_NumberOfTasksEqualToWorkers(t *testing.T) {
-	const THREADS_COUNT = 8
-	p := NewPool(THREADS_COUNT)
+	const threadCount = 8
+	p := NewPool(displayMetrics, threadCount)
 
 	var makeSureThatAllTasksExecuted atomic.Int32
 	var sleepDuration = 2000 * time.Millisecond
 
-	for index := 0; index < THREADS_COUNT; index++ {
+	for index := 0; index < threadCount; index++ {
 		p.SubmitTask(func() {
 			makeSureThatAllTasksExecuted.Add(1)
 			time.Sleep(sleepDuration)
@@ -47,23 +50,23 @@ func TestThreadPoo_NumberOfTasksEqualToWorkers(t *testing.T) {
 	p.ProcessSubmittedTasks()
 	var elapsed = time.Since(start)
 
-	assert.EqualValues(t, makeSureThatAllTasksExecuted.Load(), THREADS_COUNT)
-	assert.EqualValues(t, p.tasksSubmitted, THREADS_COUNT)
-	assert.EqualValues(t, p.waitingTasksCount, 0)
+	assert.EqualValues(t, makeSureThatAllTasksExecuted.Load(), threadCount)
+	assert.EqualValues(t, p.metrics.tasksSubmitted, threadCount)
+	assert.EqualValues(t, p.metrics.tasksQueued, 0)
 	assert.Less(t, elapsed, 3000*time.Millisecond) // maybe 3000 is too strict, bump it to 4000 if the test starts to fail.
 	assert.True(t, p.tasksQueue.Empty())
 	assert.True(t, p.waitingQueue.Empty())
 }
 
 func TestThreadPoo_NumberOfTasksIsGreaterThenWorkerCount(t *testing.T) {
-	const THREADS_COUNT = 4
-	const TASKS_COUNT = 16
-	p := NewPool(THREADS_COUNT)
+	const threadCount = 4
+	const tasksCount = 16
+	p := NewPool(displayMetrics, threadCount)
 
 	var makeSureThatAllTasksExecuted atomic.Int32
 	var sleepDuration = 2000 * time.Millisecond
 
-	for index := 0; index < TASKS_COUNT; index++ {
+	for index := 0; index < tasksCount; index++ {
 		p.SubmitTask(func() {
 			makeSureThatAllTasksExecuted.Add(1)
 			time.Sleep(sleepDuration)
@@ -74,9 +77,9 @@ func TestThreadPoo_NumberOfTasksIsGreaterThenWorkerCount(t *testing.T) {
 	p.ProcessSubmittedTasks()
 	var elapsed = time.Since(start)
 
-	assert.EqualValues(t, makeSureThatAllTasksExecuted.Load(), TASKS_COUNT)
-	assert.EqualValues(t, p.tasksSubmitted, TASKS_COUNT)
-	assert.EqualValues(t, p.waitingTasksCount, 12)
+	assert.EqualValues(t, makeSureThatAllTasksExecuted.Load(), tasksCount)
+	assert.EqualValues(t, p.metrics.tasksSubmitted, tasksCount)
+	assert.EqualValues(t, p.metrics.tasksQueued, 12)
 	assert.Less(t, elapsed, 9000*time.Millisecond)
 	assert.True(t, p.tasksQueue.Empty())
 	assert.True(t, p.waitingQueue.Empty())
